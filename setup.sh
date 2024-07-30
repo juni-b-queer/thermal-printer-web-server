9bfda9aacd0754ece017a5727f1bf8418830a8fc
Juni! <june@juniper-bender.tech>
2024-07-30 14:57:11 -0500

    add setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-sudo apt-get install python-dev
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install python-dev -y
 
 sudo pip3 install -r requirements.txt --break-system-packages
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel --break-system-packages
